cf5f96a7cdebf0318876a53b91261f9009be19d9
kisayama <163150888+kisayama@users.noreply.github.com>
2024-12-28 21:22:28 +0900

    初期追加

---
+++ app/src/main/java/com/example/cycletracker/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.cycletracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cycletracker.ui.theme.CycleTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CycleTrackerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Row {
+sd
+                    }
+                }
+            }
+        }
+    }
+}
