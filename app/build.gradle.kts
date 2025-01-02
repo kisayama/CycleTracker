ea8ad4dfe1749df6f82e9f01d7ed110711ae4006
kisayama <163150888+kisayama@users.noreply.github.com>
2025-01-02 14:57:41 +0900

    修正：compileSDKを34から35に変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.cycletracker"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.cycletracker"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
